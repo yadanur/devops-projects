e19990e0c23a10cca344c933a97d9fe78dbd1cfa
yadanur <y.adanur@gmail.com>
2021-11-27 22:29:09 -0800

    some more done

---
+++ hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+def head():
+  return "Hello from the other side!"
+
+@app.route("/login")
+def login():
+  return "This is the login page"
+
+@app.route("/login/error")
+def login_error():
+  return "Wrong password"
+
+@app.route("/login/<string:userName>")
+def loginName(userName):
+  return f"Welcome back {userName}"
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
