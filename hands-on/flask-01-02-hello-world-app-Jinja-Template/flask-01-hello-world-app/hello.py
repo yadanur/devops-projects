f2fb3cdcee4395c77497d0f0b31c849fd3914a1e
yadanur <y.adanur@gmail.com>
2021-11-27 22:41:01 -0800

    sdf

--- hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello.py
+++ hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello.py
@@ -20,4 +20,6 @@ def loginName(userName):
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=80)
+
+#  app.run(debug=True)
--- hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -15,4 +15,5 @@ def home():
   return ("this is home page")
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host='0.0.0.0', port=80)
+  #app.run(debug=True)
